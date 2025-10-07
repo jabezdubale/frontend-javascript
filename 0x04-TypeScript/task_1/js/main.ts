8d69f23e9d0dc2671feafbea54b8e7f04e507d92
jabezdubale <jabezdubale6@gmail.com>
2025-10-07 13:55:52 +0300

    6. Creating functions specific to employees

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,41 @@
 interface Teacher {
-  readonly firstName?: string;
-  readonly lastName?: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience: number;
+  yearsOfExperience?: number;
   location: string;
   [key: string]: any;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
+  return `${firstName}. ${lastName}`;
+}
+
+interface constructorInterface {
+  firstName: string;
+  lastName: string;
+}
+class StudentClass {
+  constructor(con: constructorInterface) {
+    this.firstName = con.firstName;
+    this.lastName = con.lastName;
+  }
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
 }
